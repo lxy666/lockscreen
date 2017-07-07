f0987ac175c51b4c7e4978eef71335fe93e22af8
yu <1175058580@qq.com>
2017-07-07 20:47:55 +0800

    first commit

---
+++ static/javascript/index.js
@@ -0,0 +1,44 @@
+var mons = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+$(function () {
+    var myDate = new Date();
+    var hours = myDate.getHours();
+    var minutes = myDate.getMinutes();
+    var day = myDate.getDay();
+    var date = myDate.getDate();
+    var month = myDate.getMonth();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    $("#showDate1").html(hours + ":" + minutes);
+    $("#showDate2").html(days[day] + "," + mons[month] + " " + date);
+
+    if (hours > 7 && hours < 19) {
+        $("#main").addClass("day");
+    } else {
+        $("#main").addClass("night");
+    }
+
+    setInterval("getTime();", 1000);
+})
+
+function getTime() {
+    var myDate = new Date();
+    var hours = myDate.getHours();
+    var minutes = myDate.getMinutes();
+    var day = myDate.getDay();
+    var date = myDate.getDate();
+    var month = myDate.getMonth();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    $("#showDate1").html(hours + ":" + minutes);
+    $("#showDate2").html(days[day] + "," + mons[month] + " " + date);
+
+    if (hours > 7 && hours < 19) {
+        $("#main").addClass("day");
+    } else {
+        $("#main").addClass("night");
+    }
+}
