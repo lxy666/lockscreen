16102e613f083de65658ccc3dd2c049d4d4c2639
I anticipate <1175058580@qq.com>
2017-07-08 09:22:08 +0800

    Update index.js

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -2,24 +2,7 @@ var mons = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 $(function () {
-    var myDate = new Date();
-    var hours = myDate.getHours();
-    var minutes = myDate.getMinutes();
-    var day = myDate.getDay();
-    var date = myDate.getDate();
-    var month = myDate.getMonth();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    $("#showDate1").html(hours + ":" + minutes);
-    $("#showDate2").html(days[day] + "," + mons[month] + " " + date);
-
-    if (hours > 7 && hours < 19) {
-        $("#main").addClass("day");
-    } else {
-        $("#main").addClass("night");
-    }
-
+    getTime();
     setInterval("getTime();", 1000);
 })
 
